11cca95d9c3f8c1975e36535e1f1530f87d2a5e1
NUMCHCOMCH <anstmdwn34@gmail.com>
2024-07-16 15:13:36 +0900

    first commit

---
+++ .ipynb_checkpoints/generator-openai-train-checkpoint.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+import pandas as pd
+import openai
+from vllm import LLM, SamplingParams
+from langchain.schema import HumanMessage
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help=' : Model to evaluate', default='사용 할 모델명')
+parser.add_argument('--model_len', help=' : Maximum Model Length', default=4096, type=int)
+args = parser.parse_args()
+
+client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "open api key"))
+
+df_questions = pd.read_json('FinBench_Train.jsonl', lines=True)
+
+single_turn_outputs = []
+
+for question in df_questions['questions']:
+    messages = [
+    {"role": "system", "content": 'You are an AI assistant. You will be given a task. You must generate a detailed and long answer.'},
+    {"role": "user", "content": str(question)}]
+    response = client.chat.completions.create(
+        model=args.model,
+        messages=messages,
+        max_tokens=args.model_len,
+    )
+    single_turn_outputs.append(response.choices[0].message.content)
+
+df_output = pd.DataFrame({
+    'id': df_questions['id'],
+    'category': df_questions['category'],
+    'questions': df_questions['questions'],
+    'outputs': list(zip(single_turn_outputs)),
+    'references': df_questions['references']
+})
+
+df_output.to_json(
+    f'{str(args.model).replace("/", "_")}.jsonl',
+    orient='records',
+    lines=True,
+    force_ascii=False
+)
---
+++ score-single.py
@@ -0,0 +1,43 @@
+import json
+import glob
+import argparse
+
+# 파일 경로 패턴
+# file_pattern = './judge_20240418_103542.jsonl'
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--print', help='judge Output File Location', default=None)
+args = parser.parse_args()
+
+if args.print is None:
+    raise ValueError('Judge Output File Location is required')
+
+# 카테고리별 점수 집계를 위한 딕셔너리
+category_scores = {}
+
+# 전체 싱글 점수와 멀티 점수의 리스트
+total_single_scores = []
+
+# 지정된 패턴에 맞는 모든 파일을 찾아서 처리
+for file_path in glob.glob(args.print):
+    with open(file_path, 'r', encoding='utf-8-sig') as file:  # 'utf-8-sig'로 인코딩 변경
+        for line in file:
+            item = json.loads(line)
+            category = item['category']
+            single_score = item['query_single']['judge_score']
+
+            if category not in category_scores:
+                category_scores[category] = {'single_scores': []}
+
+            category_scores[category]['single_scores'].append(single_score)
+
+            # 전체 점수 리스트에 추가
+            total_single_scores.append(single_score)
+
+# 카테고리별 평균 점수 계산
+for category, scores in category_scores.items():
+    avg_single = sum(scores['single_scores']) / len(scores['single_scores'])
+    print(f"카테고리: {category}, 싱글 점수 평균: {avg_single:.2f}")
+
+# 전체 점수의 평균 계산 및 출력
+avg_total_single = sum(total_single_scores) / len(total_single_scores)
+print(f"전체 싱글 점수 평균: {avg_total_single:.2f}")
