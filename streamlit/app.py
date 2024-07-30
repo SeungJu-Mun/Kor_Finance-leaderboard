31674bdf42a70876ea6b909e3c795598ea3f5f16
SeungJu Mun <136792663+NUMCHCOMCH@users.noreply.github.com>
2024-07-30 10:48:23 +0900

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -182,7 +182,7 @@ def setup_about():
                     json_output = df_output.to_json(orient='records', lines=True, force_ascii=False)
                     st.session_state['json_output'] = json_output
                     st.session_state['selected_option_name'] = selected_option_name
-                    upload_to_github("토큰 입력", "CPM-AI/Kor_Finance-leaderboard", f"./data/{st.session_state['selected_option_name'].replace('/', '_')}.json", json_output)
+                    upload_to_github(api_key, "CPM-AI/Kor_Finance-leaderboard", f"./data/{st.session_state['selected_option_name'].replace('/', '_')}.json", json_output)
 
         if 'json_output' in st.session_state:
             st.download_button(
