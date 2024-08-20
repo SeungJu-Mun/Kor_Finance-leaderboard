0ba8b0ac93d034fdd17f247c5c47b953b992693e
pepperonipizza97 <129192568+pepperonipizza97@users.noreply.github.com>
2024-08-20 12:54:17 +0900

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -337,6 +337,7 @@ print(finetuning_response)
         df = process_file_to_dataframe(file_path)
         df_2 = process_file_to_dataframe(file_path2)
         df_3 = process_file_to_dataframe(file_path3)
+        df_4 = process_file_to_dataframe(file_path4)
         df = pd.concat([df,df_2,df_3]).sort_values('AVG_Score',ascending=False).reset_index(drop=True)
         df['모델 제출일시'] = now = datetime.datetime.now().strftime("%Y.%m.%d") +' '+ df['모델 제출일시'] + ':00'
         df = df[['팀이름','MMLU_F','FIQUSA','MATHQA','AVG_Score','모델 제출일시']]
